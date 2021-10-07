197ce7f09d52ce3a232752965adaf863f9326aa1
Ernest <iendeavor.github.io@gmail.com>
2021-10-07 21:20:34 +0800

    feat: add Popper component

---
+++ src/MaybeTransition.vue
@@ -0,0 +1,21 @@
+<template>
+  <template v-if="transitionProps">
+    <Transition v-bind="transitionProps">
+      <slot />
+    </Transition>
+  </template>
+
+  <template v-else>
+    <slot />
+  </template>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, TransitionProps } from "vue";
+
+export default defineComponent({
+  props: {
+    transitionProps: Object as PropType<TransitionProps>,
+  },
+});
+</script>
---
+++ src/Popper.vue
@@ -0,0 +1,128 @@
+<template>
+  <component
+    :is="popperIs"
+    v-bind="popperProps"
+    ref="popper"
+    class="popper"
+    :aria-describedby="role"
+  >
+    <slot />
+  </component>
+
+  <MaybeTeleport :teleport-props="teleportProps">
+    <MaybeTransition :transition-props="transitionProps">
+      <component
+        :is="referenceIs"
+        v-bind="referenceProps"
+        v-show="visible"
+        ref="reference"
+        class="reference"
+        :role="role"
+      >
+        <slot name="reference" />
+      </component>
+    </MaybeTransition>
+  </MaybeTeleport>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  getCurrentInstance,
+  ref,
+  computed,
+  watch,
+  PropType,
+  TeleportProps,
+  TransitionProps,
+} from "vue";
+import { usePopperjs } from "./hook";
+import MaybeTeleport from "./MaybeTeleport.vue";
+import MaybeTransition from "./MaybeTransition.vue";
+
+export default defineComponent({
+  components: {
+    MaybeTeleport,
+    MaybeTransition,
+  },
+
+  props: {
+    // hook options
+    delayOnMouseout: Number,
+    delayOnMouseover: Number,
+    trigger: String as PropType<
+      Exclude<
+        Required<Parameters<typeof usePopperjs>>["2"]["trigger"],
+        "manual"
+      >
+    >,
+    forceShow: Boolean,
+    modifiers: Array as PropType<
+      Required<Parameters<typeof usePopperjs>>["2"]["modifiers"]
+    >,
+    onFirstUpdate: Function as PropType<
+      Required<Parameters<typeof usePopperjs>>["2"]["onFirstUpdate"]
+    >,
+    placement: String as PropType<
+      Required<Parameters<typeof usePopperjs>>["2"]["placement"]
+    >,
+    strategy: String as PropType<
+      Required<Parameters<typeof usePopperjs>>["2"]["strategy"]
+    >,
+
+    // component props
+    popperIs: {
+      default: "div",
+      type: String,
+    },
+    popperProps: {
+      type: Object,
+    },
+    referenceIs: {
+      default: "div",
+      type: String,
+    },
+    referenceProps: {
+      type: Object,
+    },
+    disabled: Boolean,
+    teleportProps: Object as PropType<TeleportProps>,
+    transitionProps: Object as PropType<TransitionProps>,
+  },
+
+  emits: ["show", "hide"],
+
+  setup(props, { emit }) {
+    const instance = getCurrentInstance();
+    const popper = ref();
+    const reference = ref();
+    const { visible } = usePopperjs(popper, reference, {
+      ...props,
+      onShow: () => emit("show"),
+      onHide: () => emit("hide"),
+    });
+
+    watch(
+      () => [visible.value, props.disabled],
+      () => {
+        if (props.disabled && visible.value) {
+          visible.value = false;
+        }
+      },
+      { flush: "sync" }
+    );
+
+    const role = computed(() =>
+      visible.value ? "reference-" + instance?.uid : undefined
+    );
+
+    return {
+      instance,
+      popper,
+      reference,
+      visible,
+      role,
+    };
+  },
+});
+</script>
