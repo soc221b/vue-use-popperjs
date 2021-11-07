db37debda8d30cc954b001c8c389510adbf67e84
Ernest <iendeavor.github.io@gmail.com>
2021-11-07 15:12:11 +0800

    test: use vue test utils

--- tests/popper.test.ts
+++ tests/popper.test.ts
@@ -1,6 +1,6 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref, nextTick, Ref } from "vue";
 import { mount } from "@vue/test-utils";
-import { Popper } from "..";
+import { Popper, Trigger, usePopperjs } from "..";
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const waitForFinishAnimation = () => wait(200);
@@ -107,50 +107,6 @@ describe("Popper", () => {
     `);
   });
 
-  test("it should show popper when hover reference", async () => {
-    const wrapper = mount({
-      components: { Popper },
-      template: `
-      <Popper :reference-props="{ id: 'reference' }">
-        <template #reference>My reference</template>
-        My popper
-      </Popper>
-    `.trim(),
-    });
-    await waitForFinishAnimation();
-
-    await wrapper.get("#reference").trigger("mouseover");
-    await waitForFinishAnimation();
-
-    expect(wrapper.html()).toMatchInlineSnapshot(`
-      "<div id=\\"reference\\">My reference</div>
-      <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
-    `);
-  });
-
-  test("it should hide popper after hover reference", async () => {
-    const wrapper = mount({
-      components: { Popper },
-      template: `
-      <Popper :reference-props="{ id: 'reference' }">
-        <template #reference>My reference</template>
-        My popper
-      </Popper>
-    `.trim(),
-    });
-    await waitForFinishAnimation();
-    await wrapper.get("#reference").trigger("mouseover");
-    await waitForFinishAnimation();
-
-    await wrapper.get("#reference").trigger("mouseout");
-    await waitForFinishAnimation();
-
-    expect(wrapper.html()).toMatchInlineSnapshot(`
-      "<div id=\\"reference\\">My reference</div>
-      <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px); display: none;\\" class=\\"vue-use-popperjs-none\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
-    `);
-  });
-
   describe("Teleport", () => {
     beforeEach(() => {
       // create teleport target
@@ -196,6 +152,208 @@ describe("Popper", () => {
       ).toMatchInlineSnapshot(
         `"<div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"><div>My popper</div></div>"`
       );
+    });
+  });
+
+  describe("trigger", () => {
+    test("hover", async () => {
+      const wrapper = mount({
+        components: { Popper },
+        template: `
+        <Popper trigger="hover" :reference-props="{ id: 'reference' }">
+          <template #reference>My reference</template>
+          My popper
+        </Popper>
+      `.trim(),
+      });
+      await waitForFinishAnimation();
+
+      await wrapper.get("#reference").trigger("mouseover");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+
+      await wrapper.get("#reference").trigger("mouseout");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px); display: none;\\" class=\\"vue-use-popperjs-none\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+    });
+
+    test("click-to-open", async () => {
+      const wrapper = mount({
+        components: { Popper },
+        template: `
+        <Popper trigger="click-to-open" :reference-props="{ id: 'reference' }">
+          <template #reference>My reference</template>
+          My popper
+        </Popper>
+      `.trim(),
+      });
+      await waitForFinishAnimation();
+
+      await wrapper.get("#reference").trigger("click");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+
+      await wrapper.get("#reference").trigger("click");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+    });
+
+    test("click-to-toggle", async () => {
+      const wrapper = mount({
+        components: { Popper },
+        template: `
+        <Popper trigger="click-to-toggle" :reference-props="{ id: 'reference' }">
+          <template #reference>My reference</template>
+          My popper
+        </Popper>
+      `.trim(),
+      });
+      await waitForFinishAnimation();
+
+      await wrapper.get("#reference").trigger("click");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+
+      await wrapper.get("#reference").trigger("click");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px); display: none;\\" class=\\"vue-use-popperjs-none\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+    });
+
+    test("focus", async () => {
+      const wrapper = mount({
+        components: { Popper },
+        template: `
+        <Popper trigger="focus" :reference-props="{ id: 'reference' }">
+          <template #reference>My reference</template>
+          My popper
+        </Popper>
+      `.trim(),
+      });
+      await waitForFinishAnimation();
+
+      await wrapper.get("#reference").trigger("focus");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+
+      await wrapper.get("#reference").trigger("blur");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px); display: none;\\" class=\\"vue-use-popperjs-none\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+    });
+
+    test("manual", async () => {
+      let visible: Ref<boolean>;
+      const wrapper = mount({
+        components: { Popper },
+        setup() {
+          const reference = ref();
+          const popper = ref();
+          visible = usePopperjs(reference, popper, {
+            trigger: "manual",
+          }).visible;
+
+          return {
+            reference,
+            popper,
+          };
+        },
+        template: `
+        <div ref="reference">My reference</div>
+        <div ref="popper">My popper</div>
+      `.trim(),
+      });
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div>My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"vue-use-popperjs-none\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\">My popper</div>"
+      `);
+
+      visible!.value = true;
+      await nextTick();
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div>My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\">My popper</div>"
+      `);
+
+      visible!.value = false;
+      await nextTick();
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div>My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"vue-use-popperjs-none\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\">My popper</div>"
+      `);
+    });
+
+    test("dynamic", async () => {
+      const trigger = ref<Trigger>("hover");
+      const wrapper = mount({
+        components: { Popper },
+        setup() {
+          return { trigger };
+        },
+        template: `
+        <Popper :trigger="trigger" :reference-props="{ id: 'reference' }">
+          <template #reference>My reference</template>
+          My popper
+        </Popper>
+      `.trim(),
+      });
+      await waitForFinishAnimation();
+
+      await wrapper.get("#reference").trigger("mouseover");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+
+      await wrapper.get("#reference").trigger("mouseout");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px); display: none;\\" class=\\"vue-use-popperjs-none\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+
+      trigger.value = "click-to-open";
+      await nextTick();
+
+      await wrapper.get("#reference").trigger("mouseover");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px); display: none;\\" class=\\"vue-use-popperjs-none\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
+
+      await wrapper.get("#reference").trigger("click");
+      await waitForFinishAnimation();
+      expect(wrapper.html()).toMatchInlineSnapshot(`
+        "<div id=\\"reference\\">My reference</div>
+        <div style=\\"position: absolute; left: 0px; top: 0px; margin: 0px; transform: translate(0px, 0px);\\" class=\\"\\" data-popper-reference-hidden=\\"\\" data-popper-escaped=\\"\\" data-popper-placement=\\"bottom\\"> My popper </div>"
+      `);
     });
   });
 });
